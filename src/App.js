a87d7d104bb69b026cb33b945d1f34e4998fb25c
Remidemic <jeremy.r.guzman@gmail.com>
2023-03-02 17:27:10 -0500

    gh-pages v18

--- src/App.js
+++ src/App.js
@@ -5,65 +5,77 @@ import BSNavbar from './components/Header/BSNavbar';
 import BSSchoolFacts from './components/Majors/BSSchoolFacts';
 import BSMajorList from './components/Majors/BSMajorList'
 import ResultsList from './components/Search/ResultsList';
+import Search from './components/Search/Search';
 function App() {
 
-  const MY_KEY = process.env.REACT_APP_API_KEY
-  const [data, setData] = useState({})
-  const [school, setSchool] = useState('')
-  const [results, setResults] = useState('')
+  // const MY_KEY = process.env.REACT_APP_API_KEY
+  // const [data, setData] = useState({})
+  // const [school, setSchool] = useState('')
+  // const [results, setResults] = useState('')
+  const [results_, setResults_] = useState('')
+  const [data_, setData_] = useState({})
 
-  // const [major, setMajor] = useState({})
-  // const [key, setKey] = useState({})
-
-  const url = `https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=${school}&api_key=${MY_KEY}`
-
-  const searchSchool = (event) => {
-
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        if (response.data.metadata.total > 0) {
-          setData(response.data)
-          setResults('');
-          console.log(response.data.results[0].latest)
-          console.log(response.data.metadata.total)
-        } else {
-          setResults(`no results for " ${school} " try another search`);
-          setData({});
-          return;
-        }
-      })
-    }
+  const setData_Handler = (data_) => {
+    setData_(data_)
+  }
+  const setResults_Handler = (results_) => {
+    setResults_(results_)
   }
 
-  // const setDataHandler = ({ data }) => {
-  //   setData({ data });
+  // data_ !== null ?  console.log(data_) : console.log(null);
+  // data ?   console.log(data) : console.log(null);
+
+  // const url = `https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=${school}&api_key=${MY_KEY}`
+
+  // const searchSchool = (event) => {
+  //   // console.log(event.target.value)
+
+  //   if (event.key === 'Enter') {
+  //     axios.get(url).then((response) => {
+  //       if (response.data.metadata.total > 0) {
+  //         setData(response.data)
+  //         setResults('');
+  //         console.log(response.data.metadata.total)
+  //         console.log(response.data.results[0].latest)
+  //       } else {
+  //         setResults(`no results for " ${school} " try another search`);
+  //         setData({});
+  //         return;
+  //       }
+  //     })
+  //   }
   // }
 
 
-
+  // const setSchoolChangeHandler = event => {
+  //   setSchool(event.target.value)
+  // }
 
   return (
     <div className="App">
       <BSNavbar />
-
+      <Search onSetData_={setData_Handler} onSetResults_={setResults_Handler} />
+      {/* 
       <div className="search">
         <input
-          value={school}
-          onChange={event => setSchool(event.target.value)}
-          onKeyDown={searchSchool}
           placeholder='Enter School'
-          type="text" />
+          type="text"
+          onChange={setSchoolChangeHandler}
+          onKeyDown={searchSchool}
+        />
       </div>
+      <div>
+        <button className="search">search</button>
+      </div> */}
 
-      <ResultsList data={data} />
-      <p>{results}</p>
-      {/* <br /> */}
-
-      { <BSSchoolFacts data={data} />}
+      <ResultsList data={data_} />
+      <p>{results_}</p>
+      <BSSchoolFacts data={data_} />
       <br />
-      <BSMajorList data={data} />
+      <BSMajorList data={data_} />
     </div>
   );
+
 }
 
 export default App;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,42 +1,58 @@
-
+import './../../App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
-
-const Search = (props) => {
+function Search(props) {
 
     const MY_KEY = process.env.REACT_APP_API_KEY
-    // const [data, setData] = useState({})
-
     const [data, setData] = useState({})
     const [school, setSchool] = useState('')
-
-
-    // add latest year hardcoded into query string. check docs to see how (2019? 2020?)
+    const [results, setResults] = useState('')
 
     const url = `https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=${school}&api_key=${MY_KEY}`
 
     const searchSchool = (event) => {
+
         if (event.key === 'Enter') {
             axios.get(url).then((response) => {
-                // setData(response.data)
-                props.onDataHandler(response.data)
-                console.log(response.data.results[0].latest)
+                if (response.data.metadata.total > 0) {
+                    setData(response.data)
+                    setResults('');
+                    props.onSetResults_('');
+                    props.onSetData_(response.data)
+                    console.log(response.data.metadata.total)
+                    console.log(response.data.results[0].latest)
+                } else {
+                    setResults(`no results for " ${school} " try another search`);
+                    props.onSetResults_(`no results for " ${school} " try another search`)
+                    setData({});
+                    props.onSetData_({})
+                    return;
+                }
             })
         }
     }
 
-    return <>
-    
-        <input
-            value={school}
-            onChange={event => {setSchool(event.target.value)
-                console.log(event.target.value)
-            }}
-            onKeyDown={searchSchool}
-            placeholder='Enter School component'
-            type="text" />
-            
-    </>
+    // console.log(data.results)
+
+    const setSchoolChangeHandler = event => {
+        setSchool(event.target.value)
+    }
+
+    return (
+        <div className="App">
+
+            <div className="search">
+                <input
+                    placeholder='Search Component'
+                    type="text"
+                    onChange={setSchoolChangeHandler}
+                    onKeyDown={searchSchool}
+                />
+            </div>
+
+        </div>
+    );
+
 }
 
-export default Search
+export default Search;
